c8be82b6e1eb07aa419a9445074b94fc9ca86edc
Elizaveta Shevtsova <111qw95@gmail.com>
2024-02-06 22:42:30 +0300

    base project settings

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css'
+import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css'
+
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1 className='text text_type_main-default'>Hi</h1>
+        <Logo />
+      </div>
+    </header>
+  )
+}
+
+export default Header
