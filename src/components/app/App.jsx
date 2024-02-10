59a16b8cce5026394da4ee3508236d291d7d9ef2
Elizaveta Shevtsova <111qw95@gmail.com>
2024-02-10 22:12:16 +0300

    modal children

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,7 +6,6 @@ import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css'
 import Header from '../header/Header';
 import BurgerIngredients from '../burgerIngredients/BurgerIngredients';
 import BurgerConstructor from '../burgerConstructor/BurgerConstructor';
-import Modal from '../modal/Modal'
 
 // import { data } from '../../utils/data'
 import ingredientsService from '../../utils/api';
@@ -40,8 +39,6 @@ function App() {
           </div>
         </div>
       </main>
-
-      <Modal />
     </>
   );
 }
--- src/components/burgerIngredients/BurgerIngredients.jsx
+++ src/components/burgerIngredients/BurgerIngredients.jsx
@@ -3,6 +3,7 @@ import styles from './BurgerIngredients.module.css'
 import { Tab, CurrencyIcon, Counter } from "@ya.praktikum/react-developer-burger-ui-components"
 import { useState, useEffect } from "react"
 
+import Modal from '../modal/Modal'
 
 const BurgerIngredients = ({ ingredients }) => {
   const [current, setCurrent] = useState(0)
@@ -11,49 +12,54 @@ const BurgerIngredients = ({ ingredients }) => {
   }, [])
 
   return (
-    <div className="col flex flex-col gap-10">
-      <ul className="flex">
-        {ingredients.map(item => {
-          return (
-            <li key={item.russianCategory}>
-              <Tab value={item.category} active={current === item.category} onClick={setCurrent}>
-                {item.russianCategory}
-              </Tab>
-            </li>
-          )
-        })}
-      </ul>
+    <>
+      <div className="col flex flex-col gap-10">
+        <ul className="flex">
+          {ingredients.map(item => {
+            return (
+              <li key={item.russianCategory}>
+                <Tab value={item.category} active={current === item.category} onClick={setCurrent}>
+                  {item.russianCategory}
+                </Tab>
+              </li>
+            )
+          })}
+        </ul>
 
-      <div className={`${styles.scrollbar} custom-scroll  flex flex-col gap-10`}>
-        {ingredients.map(item => {
-          return (
-            <section key={item.category}>
-              <h2 className="text text_type_main-medium mb-6">
-                {item.russianCategory}
-              </h2>
+        <div className={`${styles.scrollbar} custom-scroll  flex flex-col gap-10`}>
+          {ingredients.map(item => {
+            return (
+              <section key={item.category}>
+                <h2 className="text text_type_main-medium mb-6">
+                  {item.russianCategory}
+                </h2>
 
-              <ul className={`${styles.grid} pl-4 pr-4`}>
-                {item.items.map(elem => {
-                  return (
-                    <li key={elem._id}>
-                      <div className={`${styles.card} p-4`}>
-                        <Counter count={1} size="default" extraClass="m-1" />
-                        <img src={elem.image} alt="" />
-                        <p className="text text_type_digits-default mt-1 mb-1 flex flex-align-center gap-2">
-                          <span>{elem.price}</span>
-                          <CurrencyIcon type="primary" />
-                        </p>
-                        <div className="text text_type_main-small">{elem.name}</div>
-                      </div>
-                    </li>
-                  )
-                })}
-              </ul>
-            </section>
-          )
-        })}
+                <ul className={`${styles.grid} pl-4 pr-4`}>
+                  {item.items.map(elem => {
+                    return (
+                      <li key={elem._id}>
+                        <div className={`${styles.card} p-4`}>
+                          <Counter count={1} size="default" extraClass="m-1" />
+                          <img src={elem.image} alt="" />
+                          <p className="text text_type_digits-default mt-1 mb-1 flex flex-align-center gap-2">
+                            <span>{elem.price}</span>
+                            <CurrencyIcon type="primary" />
+                          </p>
+                          <div className="text text_type_main-small">{elem.name}</div>
+                        </div>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </section>
+            )
+          })}
+        </div>
       </div>
-    </div>
+
+      <Modal />
+    </>
+
   )
 }
 
