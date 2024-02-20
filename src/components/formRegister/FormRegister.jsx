0d9a09f3f0784aad6d1722d41411f579b477df44
Elizaveta Shevtsova <111qw95@gmail.com>
2024-02-20 08:35:46 +0300

    feat: add reg

--- src/components/formRegister/FormRegister.jsx
+++ src/components/formRegister/FormRegister.jsx
@@ -1,22 +1,44 @@
 import styles from './FormRegister.module.css'
+import { API } from '../../utils/apiConsts'
 
 import { Button, EmailInput, Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components'
-
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const FormRegister = () => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState({ email: '', password: '', name: '' })
+  const [isFullfilled, setisFullfilled] = useState(false)
+
   const onChange = e => {
-    setValue(e.target.value)
+    setValue({ ...value, [e.target.name]: e.target.value })
   }
 
+  const onSubmit = async e => {
+    e.preventDefault();
+    const result = await fetch(API._register, {
+      method: 'POST', headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      }, body: JSON.stringify(value)
+    })
+
+    console.log(value);
+  }
+
+  useEffect(() => {
+    let check = false
+    for (let key in value) {
+      check = value[key].length
+    }
+    setisFullfilled(check)
+
+  }, [value])
+
   return (
-    <form className={`${styles.form} flex flex-col gap-6 mb-10`}>
+    <form onSubmit={onSubmit} className={`${styles.form} flex flex-col gap-6 mb-10`}>
       <Input
         type={'text'}
         placeholder={'Имя'}
-        onChange={e => setValue(e.target.value)}
-        value={value}
+        onChange={onChange}
+        value={value.name}
         name={'name'}
         error={false}
         errorText={'Ошибка'}
@@ -25,18 +47,23 @@ const FormRegister = () => {
 
       <EmailInput
         onChange={onChange}
-        value={value}
+        value={value.email}
         name={'email'}
         placeholder="E-mail"
       />
 
       <PasswordInput
         onChange={onChange}
-        value={value}
+        value={value.password}
         name={'password'}
       />
-      <Button htmlType="button" >Зарегистрироваться</Button>
 
+      <Button
+        disabled={!isFullfilled}
+        htmlType="submit"
+      >
+        Зарегистрироваться
+      </Button>
     </form>
   )
 }
--- src/utils/apiConsts.js
+++ src/utils/apiConsts.js
@@ -2,8 +2,6 @@ export const API = {
   _ingredients: 'https://norma.nomoreparties.space/api/ingredients',
   _orders: 'https://norma.nomoreparties.space/api/orders',
 
-  _passwordForgot: 'https://norma.nomoreparties.space/api/password-reset',
-  _passwordReset: 'https://norma.nomoreparties.space/api/password-reset/reset',
   _register: 'https://norma.nomoreparties.space/api/auth/register',
   _login: 'https://norma.nomoreparties.space/api/auth/login',
   _logout: 'https://norma.nomoreparties.space/api/auth/logout',
